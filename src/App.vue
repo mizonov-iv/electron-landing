0fa8bdb0b53aabde8b5bb22e8489b1efa0071503
mizonov-iv <mizonov.iv@gmail.com>
2023-03-22 18:18:34 +0500

    navbar and hero

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-fluid"></div>
+  <Navbar/>
+  <Hero/>
+</template>
+
+<script setup>
+
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+</script>
+
+
---
+++ src/components/Hero.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hero">
+    <div class="features">
+      <ul class="features__list" v-for="(item, index) in features" :key="index">
+        <li class="features__item">
+          <img class="features__item--img" :src="item.img_url" alt="">
+          <p class="features__item-text">{{item.name}}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const features = ref([
+  {
+    name: "16 лет на рэнке электроснабжения",
+    img_url: "/icons/worker.svg"
+  },
+  {
+    name: "Более 300 выполненных проектов",
+    img_url: "/icons/light.svg"
+  },
+  {
+    name: "Доступные цены",
+    img_url: "/icons/count.svg"
+  },
+  {
+    name: "РАССЧИТАТЬ СТОИМОСТЬ",
+    img_url: "/icons/calculator.svg"
+  },
+])
+
+</script>
+
+<style scoped>
+
+</style>
