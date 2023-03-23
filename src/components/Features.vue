bdbb93bf2ea7bedd47e66598b182e3f2ceb95184
mizonov <mizonov.iv@gmail.com>
2023-03-23 16:30:28 +0500

    navbar update

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="container px-4 py-5">
     <h2 class="pb-2 border-bottom text-center">Реализация проектов любой сложности под ключ от заявки до конечной сдачи работ</h2>
-    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-      <div class="feature col d-flex flex-wrap" v-for="(item, index) in features" :key="index">
-        <div class="feature-icon">
-          <img class="figure-img" :src="item.img_url" :alt="item.title">
-        </div>
-        <p>{{ item.title }}</p>
+    <div class="d-flex flex-row">
+      <div class="feature d-flex flex-column align-items-center col-md-2" v-for="(item, index) in features" :key="index">
+        <img class="features__item--img" :src="item.img_url" :alt="item.title">
+        <p class="w-25 mt-3 text-center">{{ item.title }}</p>
       </div>
     </div>
   </div>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hero">
-    <div class="features">
-      <ul class="d-flex flex-md-row justify-content-md-between flex-column justify-content-center features__list">
+    <div class="features w-75">
+      <ul class="d-flex flex-md-row justify-content-md-between flex-column justify-content-center w-100 p-0">
         <li class="d-flex justify-content-center align-items-center p-2 features__item" v-for="(item, index) in features" :key="index">
           <img class="features__item--img" :src="item.img_url" :alt="item.name">
           <p class="features__item-text">{{item.name}}</p>
