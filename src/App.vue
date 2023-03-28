006c1b69cfa835a620c0951ff92c36db9f7a2636
mizonov <mizonov.iv@gmail.com>
2023-03-28 09:59:05 +0500

    tailwind start

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="container-fluid"></div>
-  <Navbar/>
-  <Hero/>
+
+  <div>
+    <header>
+      <div>
+
+      </div>
+      <ul>
+        <li>Главная</li>
+        <li>Услуги</li>
+        <li>Наши работы</li>
+        <li>О нас</li>
+        <li>Контакты</li>
+      </ul>
+      <div>
+        <a href="tel:+79920125212">+7-992-012-52-12</a>
+        <button></button>
+      </div>
+    </header>
+  </div>
+
+
 </template>
 
 <script setup>
 
-import Navbar from "./components/Navbar.vue";
-import Hero from "./components/Hero.vue";
 </script>
-
-
