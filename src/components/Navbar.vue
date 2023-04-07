26fa3393ef317204cac0bd54922dc19ee41eca47
mizonov <mizonov.iv@gmail.com>
2023-04-07 16:50:21 +0500

    no message

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="flex items-center justify-between border-2 h-20 px-5 md:p-10">
+  <div class="flex items-center content-center justify-between border-2 h-20 px-5 md:p-10">
 
-    <div class="flex flex-row items-center w-2/5 cursor-pointer">
-      <img class="block mr-4" src="/Logo.svg" alt="logotype">
+    <div class="flex flex-row items-center w-1/5 cursor-pointer">
+      <img class="block mr-1" src="/Logo.svg" alt="logotype">
       <p class="text-2xl font-bold uppercase hidden">Electron Tamers</p>
     </div>
 
-    <div class="flex flex-col mt-4  h-20 w-3/5">
-      <p class="font-medium mb-1">Электромонтажные работы в Екатеринбурге</p>
-      <ul class="hidden md:flex flex-row uppercase">
+    <div class="flex flex-col justify-center h-20 w-4/5">
+      <p class="block text-sm font-medium md:mb-1">Электромонтажные <br class="md:hidden"> работы в Екатеринбурге</p>
+      <ul
+          class="md:flex flex-row uppercase border-2 mt-40 absolute text-right"
+          :class="[burgerToggler ? 'block': 'hidden' ]"
+      >
         <li class="nav-link">Главная</li>
         <li class="nav-link">Услуги</li>
         <li class="nav-link">Наши работы</li>
@@ -17,18 +20,24 @@
       </ul>
     </div>
 
-    <div class="w-32">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-      </svg>
-    </div>
+    <div class="flex w-1/5">
+      <div class="w-40">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+        </svg>
+      </div>
 
-    <div class="w-32">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-      </svg>
-    </div>
+      <div class="w-40" @click.prevent="burgerToggler = !burgerToggler">
+        <svg v-if="!burgerToggler" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
 
+        <svg v-if="burgerToggler" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+
+      </div>
+    </div>
 
     <div class="hidden flex flex-col items-center w-1/5">
       <a class="text-lg font-semibold" href="tel:+79920125212">+7 (992) 012-52-12</a>
@@ -82,10 +91,11 @@
 <!--  </nav>-->
 <!--</template>-->
 
-<script>
-export default {
+<script setup>
 
-}
+import {ref} from "vue";
+
+const burgerToggler = ref(false)
 </script>
 
 <style scoped>
