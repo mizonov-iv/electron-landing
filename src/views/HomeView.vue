2a15a60bd1d7e2724ffd73d921b674bb865af9f9
mizonov <mizonov.iv@gmail.com>
2023-05-24 08:50:40 +0500

    no message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <template>
   <Hero/>
   <Features/>
-      <ComplexSolutions/>
+  <ComplexSolutions/>
   <Slider/>
-  <Benefits/>
-  <Why/>
   <h2 class="mt-5 pb-2 border-bottom text-center">Оставьте эксперсс заявку</h2>
   <ContactForm/>
 </template>
@@ -13,9 +11,6 @@
 import Hero from "../components/Hero.vue";
 import Features from "../components/Features.vue";
 import ComplexSolutions from "../components/ComplexSolutions.vue";
-import Benefits from "../components/Benefits.vue";
-import Why from "../components/Why.vue";
 import Slider from "../components/Slider.vue";
 import ContactForm from "../components/ContactForm.vue";
-
 </script>
